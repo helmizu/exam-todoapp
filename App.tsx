7fb8daeaf2ca1c763c878af28e76cf87d71f531c
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2023-02-16 20:54:32 +0700

    init

---
+++ App.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/no-unstable-nested-components */
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/Home';
+import TasksScreen from './src/screens/Tasks';
+
+const Tab = createBottomTabNavigator();
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: '#4B7BE5',
+            tabBarInactiveTintColor: '#8E96A1',
+          }}>
+          <Tab.Screen
+            name="Home"
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Ionicons name="home-outline" color={color} size={size} />
+              ),
+            }}
+            component={HomeScreen}
+          />
+          <Tab.Screen
+            name="Tasks"
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Ionicons name="calendar-outline" color={color} size={size} />
+              ),
+            }}
+            component={TasksScreen}
+          />
+          <Tab.Screen
+            name="Profile"
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Ionicons name="person-outline" color={color} size={size} />
+              ),
+            }}
+            component={TasksScreen}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
---
+++ src/utils/storage.ts
@@ -0,0 +1,35 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeData = async (key: string, value: any) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getData = async (key: string) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const removeData = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const clearStorage = async () => {
+  try {
+    await AsyncStorage.clear();
+  } catch (e) {
+    console.log(e);
+  }
+};
